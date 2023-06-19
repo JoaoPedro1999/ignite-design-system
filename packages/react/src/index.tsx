dc5d5b9cdc76efa01713d34c42402d3f13f729f0
Joao Pedro Beck Land <joaopedrobeckland@gmail.com>
2023-06-19 15:04:58 -0300

    Finish Components

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,5 @@
-export * from "./components/Button";
+export { Button } from "./components/Button";
+export type { ButtonProps } from "./components/Button";
 
 export { Box } from "./components/Box";
 export type { BoxProps } from "./components/Box";
@@ -11,3 +12,15 @@ export type { HeadingProps } from "./components/Heading";
 
 export { Avatar } from "./components/Avatar";
 export type { AvatarProps } from "./components/Avatar";
+
+export { TextInput } from "./components/TextInput";
+export type { TextInputProps } from "./components/TextInput";
+
+export { TextArea } from "./components/TextArea";
+export type { TextAreaProps } from "./components/TextArea";
+
+export { Checkbox } from "./components/Checkbox";
+export type { CheckboxProps } from "./components/Checkbox";
+
+export { MultiStep } from "./components/MultiStep";
+export type { MultiStepProps } from "./components/MultiStep";
